7d484686ed288b74a92b75b2ef95e56f6f87ecb6
maggotgdv <alejandro.aguirre.k@outlook.com>
2014-05-13 23:25:25 -0500

    Libray.cs

---
+++ ProyectoHS/Library.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ProyectoHS
+{
+    public class Observer
+    {
+        private delegate void ColorEventHandler(string _color);
+        private event ColorEventHandler ColorChangedEvent;
+        private TextBox txt;
+
+        public Observer(TextBox _txt)
+        {
+            this.ColorChangedEvent += new ColorEventHandler(Observer_ColorChangedEvent);
+            this.txt = _txt;
+        }
+
+        private void OnChange(string _color)
+        {
+            if (ColorChangedEvent != null)
+            {
+                ColorChangedEvent(_color);
+            }
+        }
+
+        public void Update(string _color)
+        {
+            OnChange(_color);
+        }
+
+        private void Observer_ColorChangedEvent(string _color)
+        {
+            switch (_color)
+            {
+                case "RED":
+                    txt.BackColor = Color.Red;
+                    break;
+                case "BLUE":
+                    txt.BackColor = Color.Blue;
+                    break;
+                case "GREEN":
+                    txt.BackColor = Color.Green;
+                    break;
+                default:
+                    txt.BackColor = Color.Gray;
+                    break;
+            }
+        }
+    }
+
+    public class Subject
+    {
+        ArrayList arrObs = new ArrayList();
+        private delegate void NotifyHandler(string _color);
+        private event NotifyHandler NotifyEvent;
+
+        public Subject()
+        {
+            this.NotifyEvent += new NotifyHandler(Notify);
+        }
+
+        public void UpdateClient(string _color)
+        {
+            OnNotify(_color);
+        }
+
+        private void OnNotify(string _color)
+        {
+            if (NotifyEvent != null)
+            {
+                NotifyEvent(_color);
+            }
+        }
+
+        private void Notify(string _color)
+        {
+            for (int i = 0; i < arrObs.Count; i++)
+            {
+                Observer obs = (Observer)arrObs[i];
+                obs.Update(_color);
+            }
+        }
+
+        public void RegisterClient(Observer obs)
+        {
+            arrObs.Add(obs);
+        }
+    }
+}
