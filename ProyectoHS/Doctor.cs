f1630e4e512894895fdafc6f7827349de06a05c7
jeanvs23 <jeanvs_23@hotmail.com>
2014-06-11 14:13:35 -0500

    yuhgfdsdfgbbb

---
+++ ProyectoHS/Doctor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoHS
+{
+    class Doctor:IDoctor
+    {
+        string nombredoc;
+        public void SetNombreDoc(string inombredoc)
+        {
+            nombredoc = inombredoc;
+        }
+    }
+}
